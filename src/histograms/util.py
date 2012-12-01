d561159dc690d1aef4dc6663b913fe40b2e7fc8a
eugene wu <sirrice@gmail.com>
2012-12-01 12:07:03 -0500

    refactored

--- src/histograms/util.py
+++ src/histograms/util.py
@@ -9,14 +9,96 @@ from sqlalchemy import *
 
 re_col = re.compile('\d+$')
 
-format_iter = lambda fmt, l: (fmt % el for el in l)
 
+format_iter = lambda fmt, l: (fmt % el for el in l)
 
 class HistogramMaker(object):
     def __init__(self, db, table):
         self.db = db
         self.table = table
-        self.mapping = self.get_attr_mappings()
+
+    def __call__(self, tablename, join_cols, agg_group_cols, aggs, colrange=None):
+        """
+        @param tablename resulting tablename that histogram should be stored in
+        @param join_cols [provider]
+        @param agg_group_cols [diagnosis] 
+               Note diagnosis has multiple columns in the database
+               e.g., diag1, diag2,... diag9
+        @param aggs [(agg_func, agg_colname)]
+        """
+
+        # first create union of the columns
+        union_maker = UnionMaker(self.db, self.table)
+        union_tablename = union_maker(tablename, join_cols+agg_group_cols, aggs=aggs, colrange=colrange) 
+
+        # execute group by
+        self.execute_groupby(tablename, union_tablename, join_cols+agg_group_cols, aggs=aggs)
+
+        # normalize and return
+        norm_subquery = self.construct_norm_query(union_tablename, join_cols, aggs)
+
+        gbs = join_cols + agg_group_cols
+        gb_exprs = format_iter('t.%s as %s', zip(gbs, gbs))
+        gb_exprs = ','.join(gb_exprs)
+        agg_exprs = ["t.%s::float/norm_t.%s as %s" % (ac, ac, ac) for ag, ac in aggs]
+        agg_exprs = ','.join(agg_exprs)
+        select_clause = "SELECT %s, %s" % (gb_exprs, agg_exprs)
+        from_clause = "FROM %s as t, (%s) as norm_t" % (union_tablename, norm_subquery)
+        where_clause = "WHERE %s" % ' and '.join(format_iter("t.%s = norm_t.%s", zip(join_cols, join_cols)))
+
+        query = "%s\n%s\n%s;" % (select_clause, 
+                                 from_clause,
+                                 where_clause)
+        print query
+
+        with self.db.begin() as conn:
+            dists = defaultdict(list)
+            for row in conn.execute(query):
+                group = tuple(map(str, (row[col] for col in agg_group_cols)))
+                #vals = dict([(ac, row[ac]) for ag, ac in aggs])
+                ac = aggs[0][1]
+                dists[group].append(row[ac])
+            return dists
+        return None
+
+
+    def construct_norm_query(self, tablename, join_cols, aggs):
+        gbs = ','.join(join_cols)
+        agg_exprs = []
+        for ag, ac in aggs:
+            if ag.lower() == 'count':
+                ag = 'SUM'
+            agg_exprs.append('%s(%s) as %s' % (ag, ac, ac))
+        agg_exprs = ','.join(agg_exprs)
+        norm_query = "SELECT %s, %s FROM %s GROUP BY %s" % (gbs, agg_exprs, tablename, gbs)
+        return norm_query
+
+
+    def execute_groupby(self, tablename, union_tablename, gbs, aggs=[]):
+
+        #
+        # now execute the groupby...
+        #
+
+        sel_aggs = ['%s(%s) as %s' % (ag, ac, ac) for  ag, ac in aggs]
+        sel_cols = list(gbs) + sel_aggs
+        sel_cols = ','.join(sel_cols)
+        q = "drop table if exists %s; create table %s as select %s from %s group by %s"
+        q = q % (tablename, tablename, sel_cols, union_tablename, ','.join(gbs))
+        print q
+
+        with self.db.begin() as conn:
+            conn.execute(q)
+
+        # save this info somewhere so we can cache
+        #self.store(tablename, (gbs, aggs))
+ 
+
+class UnionMaker(object):
+    def __init__(self, db, table):
+        self.db = db
+        self.table = table
+        self.mapper = ColMapper(db, table)
         self.schemadb = bsddb3.hashopen('./groupby.db')
 
     def tablenames(self):
@@ -36,8 +118,61 @@ class HistogramMaker(object):
         self.schemadb['tablenames'] = json.dumps(tablenames)
 
 
+    def __call__(self, *args, **kwargs):
+        union_tablename = self.disambiguate_table(*args, **kwargs)
+        return union_tablename
+
+
+    def disambiguate_table(self, tablename, gbs, aggs=[], colrange=None):
+        """
+        replaces the shortname column names in @gbs with
+        the actual column names (e.g., gb = diag, actual = diag1, diag2,...)
+
+        Constructs a union
+        """
+        # first create a retardedly large table with the 
+        # cross product of all gbs
+        combos = product(*[self.mapper(col, colrange) for col in gbs])
+        agg_funcs, agg_cols = aggs and zip(*aggs) or ([], [])
+        union_tablename = '%s_union' % tablename
+
+        # XXX: not sure we should be taking the cross product of the columns...
+        big_query = []
+        first = True
+        for idx, combo in enumerate(combos):
+            sel_cols = list(format_iter('%s::text as %s', zip(combo, gbs)))
+            sel_cols.extend(agg_cols)
+            sel_cols = ','.join(sel_cols)
+            q = "(select %s from %s)" % (sel_cols, self.table)
+            
+            with self.db.begin() as conn:
+                if first:
+                    print q
+                    conn.execute("drop table if exists %s; create table %s as %s" % (union_tablename, union_tablename, q))
+                    first = False
+                else:
+                    conn.execute("insert into %s (%s)" % (union_tablename, q))
+        
+        return union_tablename
+
+
+   
+
+class ColMapper(object):
+    def __init__(self, db, table):
+        self.db = db
+        self.table = table
+        self.mapping = self.get_attr_mappings()
+
+    def __call__(self, col, colrange=None):
+        cols = self.mapping.get(col, [col])
+        if colrange and len(cols) > 1:
+            return [cols[idx] for idx in xrange(len(cols)) if idx in colrange]
+        return cols
+
     def base_cols(self):
         return self.mapping.keys()
+
 
     def get_attr_mappings(self):
         """
@@ -59,55 +194,18 @@ class HistogramMaker(object):
                     mapping[col_base].append(col)
         return mapping
 
-    def __call__(self, *args, **kwargs):
-        return self.create_histogram(*args, **kwargs)
-
-    def create_histogram(self, tablename, gbs, aggs=[]):
-        """
-        creates a histogram using aggs.
-        always adds a column called 'count' for count(*)
-        """
-        # first create a retardedly large table with the 
-        # cross product of all gbs
-        combos = product(*[self.mapping[col] for col in gbs])
-        agg_funcs, agg_cols = aggs and zip(*aggs) or ([], [])
-        union_tablename = '%s_union' % tablename
-
-        big_query = []
-        for idx, combo in enumerate(combos):
-            sel_cols = list(format_iter('%s::text as %s', zip(combo, gbs)))
-            sel_cols.extend(agg_cols)
-            sel_cols = ','.join(sel_cols)
-            q = "(select %s from %s)" % (sel_cols, self.table)
-            
-            with self.db.begin() as conn:
-                if idx == 0:
-                    conn.execute("create table %s as %s" % (union_tablename, q))
-                else:
-                    conn.execute("insert into %s (%s)" % (union_tablename, q))
 
 
-        #
-        # now execute the groupby...
-        #
 
-        sel_aggs = ['%s(%s) as %s' % (ag, ac, ac) for  ag, ac in aggs]
-        sel_cols = list(gbs)
-        sel_cols.extend(sel_aggs)
-        sel_cols = ','.join(sel_cols)
-        q = "create table %s as select %s, count(*) as count from %s group by %s"
-        q = q % (tablename, sel_cols, union_tablename, ','.join(gbs))
 
-        with self.db.begin() as conn:
-            conn.execute(q)
-
-        # save this info somewhere!
-        self.store(tablename, (gbs, aggs))
-    
-        
 if __name__ == '__main__':
     db = create_engine('postgresql://localhost/penispros')
     hm = HistogramMaker(db, 'inp')
-    print hm.base_cols()
-    hm('diagnoses', ['provider', 'dgnscd'])
-    hm('diagnoses_norm', ['provider'])
+#    hm = HistogramMaker(db, 'prov_diag_counts')
+#    res = hm('testies', ('provider',), ('dgnscd1',), [('sum', 'count')], [1])
+    res = hm('testies', ('provider',), ('dgnscd',), [('count', 'id')], range(2,10))
+
+    for x,y in res.items():
+        print x, y
+#    hm('diagnoses', ['provider', 'dgnscd'])
+#    hm('diagnoses_norm', ['provider'])
